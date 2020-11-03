c087fe36af3d584b5a59a02f650c7929e7fbd68a
Liu <yuanl0517@gmail.com>
2020-11-03 21:50:29 +0100

    add velocity command

---
+++ src/position_command/src/velocity_command_talker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
+
+def position_command_talker():
+    pub = rospy.Publisher('/drone_1/cmd_vel', Twist, queue_size=100)
+    #pub_2 = rospy.Publisher('/drone_2/command/pose', PoseStamped, queue_size=100)
+    rospy.init_node('velocity_command_talker', anonymous=True)
+    rate = rospy.Rate(100)
+    while not rospy.is_shutdown():
+        # Conmmand to drone_1
+        velocity_command = Twist()
+        velocity_command.linear.x = 0
+        velocity_command.linear.y = 0
+        velocity_command.linear.z = 0.05
+        #velocity_command.header.frame_id = "drone_1/world"
+
+        pub.publish(velocity_command)
+
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        position_command_talker()
+    except rospy.ROSInterruptException:
+        pass
